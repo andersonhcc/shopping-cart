bb1fe24ed039684506cf74f9c8df3db66b72266a
andersonhcc <programadorandersonh@gmail.com>
2022-12-23 09:49:19 -0300

    finish frontEnd basic

--- src/components/ListProducts/styles.ts
+++ src/components/ListProducts/styles.ts
@@ -32,3 +32,4 @@ export const ButtonAddCarProduct = styled.TouchableOpacity`
 
 
 `;
+
--- src/screens/MyProducts/index.tsx
+++ src/screens/MyProducts/index.tsx
@@ -3,7 +3,19 @@ import { FlatList } from 'react-native';
 
 import { useRoute } from '@react-navigation/native';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  Main,
+  Details,
+  Price,
+  AmountProduct,
+  Buttons,
+  ButtonAdd,
+  ButtonRemove,
+  TitleButton,
+  PriceAll,
+} from './styles';
 
 import { DataProducts } from '../Home';
 
@@ -17,18 +29,36 @@ export function MyProducts() {
   const { params } = useRoute();
   const { item } = params as Params;
 
-  
+
   return (
     <Container>
-      
-      <FlatList 
+
+      <FlatList
         data={item}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <Title>{item.name}</Title> 
+          <Main>
+            <Details>
+              <Title>{item.name}</Title>
+              <Price>R${item.price}</Price>
+              <AmountProduct>30</AmountProduct>
+            </Details>
+
+            <Buttons>
+              <ButtonAdd>
+                <TitleButton>+</TitleButton>
+              </ButtonAdd>
+              <ButtonRemove>                
+                <TitleButton>-</TitleButton>
+              </ButtonRemove>
+            </Buttons>
+          </Main>
+
         )}
       />
-     
+
+      <PriceAll>R$ 100</PriceAll>
+
 
     </Container>
   );
